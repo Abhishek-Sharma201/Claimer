89e83a46f8f28d1307a0b8149720f5cca574b1f0
Abhishek Sharma <abhishek.webdev001@gmail.com>
2025-03-27 12:12:46 +0530

    backend-url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const apiURL = "https://nextjs-with-auth-backend-temp.onrender.com";
+export const apiURL = "https://chm-hackverse-2025.onrender.com";
